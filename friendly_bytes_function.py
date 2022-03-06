702d2aa731647a08a9d65976a4633a56deffb01d
Shavit <61514960+shaCode256@users.noreply.github.com>
2022-03-06 14:25:04 +0200

    Add files via upload

--- friendly_bytes_function.py
+++ friendly_bytes_function.py
@@ -1,4 +1,7 @@
 import math
+#not sure about how do the keep_width and decimals parameters work together.
+#assumed that if the number is 10.02 with 2 decimal points and it's KB so there should be 
+# 3 numbers after the decimal point according to keep_width=True, so 10.020 KB
 
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
@@ -33,7 +36,7 @@ def friendly_bytes(number, decimals=2, binary=False, keep_width=False):
         numStr= str(num_by_unit)
         if numStr != None and '.' in numStr:
             lenAfterDot =len(numStr.split('.')[1])
-            zeros_string= '0'*(num_zeros-lenAfterDot)
+            zeros_string= '0'*(num_zeros-lenAfterDot+1)
             return str(num_by_unit)+zeros_string+" "+unit
     return (str(num_by_unit)+" "+unit)
 
@@ -46,50 +49,54 @@ allSizes= ( 1230000000000000000 ,4300000000000000000000,100000000000000000000000
 
 print("import pytest")
 print("from friendly_bytes_function import friendly_bytes")
-
+print("")
 print ("def test_on_positive():")
+print("")
 for num in numbers: #on positive
     friendly_num= friendly_bytes(num)
-    print("assert(friendly_bytes(",num,") == '",friendly_num,"')")
-
+    print("    assert(friendly_bytes(",num,") == '",friendly_num,"')")
+print("")
 print ("def test_on_negative():")
+print("")
 for num in numbers: #on negative
     friendly_num= friendly_bytes(-num)
-    print("assert(friendly_bytes(-",num,") == '",friendly_num,"')")
-
+    print("    assert(friendly_bytes(-",num,") == '",friendly_num,"')")
+print("")
 print ("def test_on_decimals():")
+print("")
 for num in decimals: #on decimals
     friendly_num= friendly_bytes(num)
-    print("assert(friendly_bytes(",num,") == '",friendly_num,"')")
-
+    print("    assert(friendly_bytes(",num,") == '",friendly_num,"')")
+print("")
 print("def test_decimal_parameter():")
+print("")
 for num in decimals: #on decimals
     for i in range (10):
         friendly_num= friendly_bytes(num, decimals=i)
-        print("assert (friendly_bytes(",num,",decimals=",i,") == '",friendly_num,"')")
-
+        print("    assert (friendly_bytes(",num,",decimals=",i,") == '",friendly_num,"')")
+print("")
 print ("def test_binary_parameter():")
+print("")
 for num in numbers: #on binary
     friendly_num= friendly_bytes(num, binary=True)
-    print("assert (friendly_bytes(",num,", binary=True) == '",friendly_num,"')")
-
+    print("    assert (friendly_bytes(",num,", binary=True) == '",friendly_num,"')")
+print("")
 print ("def test_false_binary_parameter():")
+print("")
 for num in numbers: #on binary
     friendly_num= friendly_bytes(num, binary=False)
-    print("assert(friendly_bytes(",num,", binary=False) == '",friendly_num,"')")
-
+    print("    assert(friendly_bytes(",num,", binary=False) == '",friendly_num,"')")
+print("")
 print ("def test_eb_to_yb_sizes():")
+print("")
 for num in allSizes: #big sizes
     for i in range (7):
         friendly_num= friendly_bytes(i*num)
-        print("assert(friendly_bytes(",i*num,", binary=False) == '",friendly_num,"')")
-
+        print("    assert(friendly_bytes(",i*num,", binary=False) == '",friendly_num,"')")
+print("")
 print ("def test_keep_width_true():")
+print("")
 for num in numbers: #big sizes
     for i in range (7):
         friendly_num= friendly_bytes(i*num, keep_width=True)
-        print("assert(friendly_bytes(",i*num,", keep_width=True) == '",friendly_num,"')")
-
-
-#print(friendly_bytes(2343234434535))
-
+        print("    assert(friendly_bytes(",i*num,", keep_width=True) == '",friendly_num,"')")
